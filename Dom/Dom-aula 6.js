6cde96286b3fe84512ef67ff828883c4e525ca93
Lucas Cunha de Leva <lucas.leva@techers.local>
2026-03-18 16:04:25 -0300

    aula DOM

---
+++ Dom/Dom-aula 6.js
@@ -0,0 +1,38 @@
+const user = {
+    Name: "Lucas de Leva",
+    age: 17,
+    EyesColor: "green",
+    HairColor: "brown",
+    SayHello: function(){
+        alert("Hello")
+    }
+}
+
+user.SayHello()
+alert(user.Name)
+
+const linhas = document.getElementsByClassName("linha-filme")
+console.log(linhas)
+
+console.log(linhas [0])
+console.log(linhas [1])
+
+console.log (linhas [0].children)
+
+for(let i = 0; i < 3; i++){
+    console.log(linhas[i])
+}
+
+for(let l = 0; l < 3; l++){
+    const celulas = linhas [l].children
+    for (let c = 0; c < 4; c++){
+        console.log(celulas[c])
+    }
+}
+
+celulas[c].textContent = 
+
+const baseDados = [
+    
+
+]
